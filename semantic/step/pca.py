77555090a49a1eb5731729386292adf33a72e2e7
fawind <windheuser.fabian@gmail.com>
2016-06-14 16:59:52 +0200

    Add PCA step

--- semantic/step/pca.py
+++ semantic/step/pca.py
@@ -1,19 +1,29 @@
+import sklearn
 from semantic.step import Step
 
 
 class PCA(Step):
 
-    def __init__(self, dimensions=50):
+    def __init__(self, dimensions=50, kernel='rbf', **kwargs):
         self._dimensions = dimensions
+        self._kernel = kernel
+        self._arguments = kwargs
 
     def fit(self, vectors):
-        pass
+        self.model = sklearn.decomposition.KernelPCA(
+            n_components=self._dimensions,
+            kernel=self._kernel,
+            **self._arguments
+        )
+        self.model.fit(vectors)
+        return self.model
 
     def transform(self, vectors):
-        pass
+        assert self.model
+        return self.model.transform(vectors)
 
     def get_params(self):
-        pass
+        return self.model
 
     def set_params(self, params):
-        pass
+        self.model = params
