817b88f6bfad66665412c29f9a42c5fe669b2813
Danijar Hafner <mail@danijar.com>
2016-06-15 00:50:00 +0200

    Rename pca to svd

--- semantic/step/tfidf.py
+++ semantic/step/tfidf.py
@@ -6,19 +6,19 @@ from semantic.parser import Reader
 
 class TfIdf(Step):
 
-    def __init__(self, pca=None, **kwargs):
+    def __init__(self, svd=None, **kwargs):
         self._vectorizer = TfidfVectorizer(**kwargs)
-        self._pca = TruncatedSVD(**(pca or {}))
+        self._svd = TruncatedSVD(**(svd or {}))
 
     def fit(self, filename):
         articles = Reader(filename)
         contents = [' '.join(x) for _, x in articles]
         vectors = self._vectorizer.fit_transform(contents)
-        self._pca.fit(vectors)
+        self._svd.fit(vectors)
 
     def transform(self, filename):
         articles = Reader(filename)
         uuids, contents = zip(*[(x, ' '.join(y)) for x, y in articles])
         vectors = self._vectorizer.transform(contents)
-        vectors = self._pca.transform(vectors)
+        vectors = self._svd.transform(vectors)
         return uuids, vectors
