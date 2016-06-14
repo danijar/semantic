8f1f0f755435106b1a0b018131d139f79353a6bd
Alexander Immer <ext-alexander.immer@here.com>
2016-06-14 17:48:06 +0200

    Implement Gaussian Mixture Model with EM

--- semantic/step/gmm.py
+++ semantic/step/gmm.py
@@ -1,16 +1,28 @@
+from sklearn.mixture import GMM as SkGMM
+from numpy import exp
+
 from semantic.step import Step
 
 
 class GMM(Step):
 
+    def __init__(self, n_components=5, n_iter=100, n_init=1, **kwargs):
+        self.n_components = n_components
+        self.n_iter = n_iter
+        self.n_init = n_init
+        self.kwargs = kwargs
+        self.gmm = None
+
     def fit(self, vectors):
-        pass
+        self.gmm = SkGMM(n_components=self.n_components, n_init=self.n_init,
+                         n_iter=self.n_init, **self.kwargs)
+        self.gmm.fit(vectors)
 
     def transform(self, vectors):
-        pass
+        return exp(self.gmm.score(vectors))
 
     def get_params(self):
-        pass
+        return self.gmm
 
     def set_params(self, params):
-        pass
+        self.gmm = params
