7cc7e3496ccaf7c4bcc35ae25fcb9b9ba9e87e04
Alexander Immer <ext-alexander.immer@here.com>
2016-06-14 21:39:29 +0200

    Remove unnecessary steps

--- semantic/step/__init__.py
+++ semantic/step/__init__.py
@@ -3,5 +3,3 @@ from .doc2vec import Doc2Vec
 from .tfidf import TfIdf
 from .gmm import GMM
 from .multinomial import Multinomial
-from .ica import ICA
-from .tsne import TSNE
--- semantic/step/step.py
+++ semantic/step/step.py
@@ -1,6 +1,4 @@
 from abc import ABCMeta, abstractmethod
-import os
-import pickle
 
 
 class Step(metaclass=ABCMeta):
@@ -13,11 +11,3 @@ class Step(metaclass=ABCMeta):
     def transform(self, data):
         raise NotImplementedError
 
-    def save(self, filepath):
-        with open(filepath, 'wb') as file_:
-            pickle.dump(self, file_)
-
-    @classmethod
-    def load(cls, filepath):
-        with open(filepath, 'rb') as file_:
-            return pickle.load(file_)
