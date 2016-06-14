0c5c7052c234a6c140c98f0d3d6bcde56a5c4b3b
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-06-14 16:49:13 +0200

    Add tf-idf implementation

--- semantic/step/tfidf.py
+++ semantic/step/tfidf.py
@@ -1,19 +1,31 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from semantic.step import Step
 
 
-class Tfidf(Step):
+class TfIdf(Step):
 
-    def __init__(self, min_occurances=3):
-        self._min_occurances = min_occurances
+    def __init__(self, min_occurrences=3, **params):
+        # TODO: min_occurrences?
+        # self._min_occurrences = min_occurrences
+        self.vectorizer = TfidfVectorizer(
+            stop_words='english',
+            max_df=0.15,
+            min_df=0.05,
+            **params
+        )
 
-    def fit(self, documents):
-        pass
+    def fit(self, articles):
+        articles = [a.content for a in articles]
+        self.vectorizer.fit(articles)
 
-    def transform(self, documents):
-        pass
+    def transform(self, articles):
+        uuids, articles = zip(*[(a.uuid, a.content) for a in articles])
+        tf_idfs = self.vectorizer.transform(articles['content'])
+        return uuids, tf_idfs
 
     def get_params(self):
-        pass
+        return self.vectorizer.get_params()
 
     def set_params(self, params):
-        pass
+        self.vectorizer.set_params(**params)
