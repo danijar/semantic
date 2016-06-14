6fd9c11637b66e70199e421db7577cc3f2e15590
Danijar Hafner <mail@danijar.com>
2016-06-14 21:52:34 +0200

    Implement k-fold validation

--- semantic/command/train/__main__.py
+++ semantic/command/train/__main__.py
@@ -1,38 +1,50 @@
 import pickle
 import os
 import definitions
+import numpy as np
+from sklearn.cross_validation import KFold
 from semantic.utility import ensure_directory
 
 
-def load_definition(root):
+ROOT = os.path.join(os.path.dirname(__file__), '../../..')
+
+
+def load_definition():
     schema = os.path.join(os.path.dirname(__file__), 'schema.yaml')
-    definition = os.path.join(root, 'definition/train.yaml')
+    definition = os.path.join(ROOT, 'definition/train.yaml')
     definition = definitions.Parser(schema)(definition)
-    definition.data = os.path.join(root, definition.data)
+    definition.data = os.path.join(ROOT, definition.data)
     return definition
 
 
-def train(distribution, vectors):
-    distribution.fit(vectors)
+def load_data(definition):
+    with open(os.path.join(ROOT, definition.vectorizer), 'rb') as file_:
+        vectorizer = pickle.load(file_)
+    _, vectors = vectorizer.transform(definition.data)
+    return vectors
 
 
-def test(distribution, vectors):
-    pass
+def training(definition, distribution, data):
+    folds = KFold(
+        data.shape[0], n_folds=definition.folds, shuffle=True, random_state=0)
+    for train, test in folds:
+        train, test = data[train], data[test]
+        distribution.fit(train)
+        predictions = distribution.transform(test)
+        yield -np.log(predictions).sum()
 
 
 def main():
-    root = os.path.join(os.path.dirname(__file__), '../../..')
-    definition = load_definition(root)
-    with open(os.path.join(root, definition.vectorizer), 'rb') as file_:
-        vectorizer = pickle.load(file_)
-    _, vectors = vectorizer.transform(definition.data)
-    # TODO: Split dataset.
+    definition = load_definition()
+    data = load_data(definition)
     for distribution in definition.distributions:
-        output = os.path.join(
-            root, definition.output, type(distribution).__name__.lower())
+        name = type(distribution).__name__.lower()
+        output = os.path.join(ROOT, definition.output, name)
         ensure_directory(output)
-        train(distribution, train_data)
-        test(distribution, test_data)
+        costs = np.array(training(distribution, data))
+        message = 'Fit {} cost mean {} variance {}'
+        print(message.format(costs.mean(), costs.std(), name))
+        distribution.fit(data)
         with open(os.path.join(output, 'distribution.pkl'), 'wb') as file_:
             pickle.dump(distribution, file_)
 
