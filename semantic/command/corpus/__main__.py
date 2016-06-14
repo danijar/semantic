14c8008502747069f7c46b6906f0bb5d4478e625
Danijar Hafner <mail@danijar.com>
2016-06-14 22:01:41 +0200

    Add training definition

--- semantic/command/corpus/__main__.py
+++ semantic/command/corpus/__main__.py
@@ -5,19 +5,27 @@ import numpy as np
 from semantic.utility import ensure_directory
 
 
-def main():
+ROOT = os.path.join(os.path.dirname(__file__), '../../../')
+
+
+def load_definition():
     schema = os.path.join(os.path.dirname(__file__), 'schema.yaml')
-    root = os.path.join(os.path.dirname(__file__), '../../../')
-    definition = os.path.join(root, 'definition/corpus.yaml')
+    definition = os.path.join(ROOT, 'definition/corpus.yaml')
     definition = definitions.Parser(schema)(definition)
-    definition.fit.filename = os.path.join(root, definition.fit.filename)
+    definition.fit.filename = os.path.join(ROOT, definition.fit.filename)
+    return definition
+
+
+def main():
+    definition = load_definition()
     for vectorizer in definition.vectorizers:
-        output = os.path.join(
-            root, definition.output, type(vectorizer).__name__.lower())
+        name = type(vectorizer).__name__.lower()
+        output = os.path.join(ROOT, definition.output, name)
         ensure_directory(output)
+        print('Fit', name)
         vectorizer.fit(**definition.fit)
         with open(os.path.join(output, 'vectorizer.pkl'), 'wb') as file_:
-            pickel.dump(vectorizer, file_)
+            pickle.dump(vectorizer, file_)
         uuids, vectors = vectorizer.transform(**definition.transform)
         np.save(os.path.join(output, 'uuids.npy'), uuids)
         np.save(os.path.join(output, 'vectors.npy'), vectors)
