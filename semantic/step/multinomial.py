d929c58ba7fc14e2165767046580a911b699c22a
Alexander Immer <ext-alexander.immer@here.com>
2016-06-15 19:07:57 +0200

    Original Multinomial, keep MultinomialAVG

--- semantic/step/multinomial.py
+++ semantic/step/multinomial.py
@@ -35,6 +35,41 @@ class MultinomialNB(Step):
         n_dim = vectors.shape[1]
         vectors = self.pipeline.transform(vectors)
         for bins in vectors:
+            pr = np.product(self.distribution[np.arange(n_dim), bins])
+            probabilities.append(pr)
+        return -np.log(np.maximum(1e-10, np.array(probabilities)))
+
+
+class MultinomialAVG(Step):
+
+    def __init__(self, percentile_threshold, bins):
+        assert bins > 0
+        bin_size = 1 / bins
+        self.bins = np.arange(bin_size, 1, bin_size)
+        self.lower = percentile_threshold
+        self.upper = 100 - percentile_threshold
+        scaler = MinMaxScaler()
+        discretizer = FunctionTransformer(Discretizer(self.bins))
+        self.pipeline = Pipeline(
+            [('scaler', scaler), ('discretizer', discretizer)])
+
+    def fit(self, vectors):
+        self.lower_clip = np.percentile(vectors, self.lower, axis=0)
+        self.upper_clip = np.percentile(vectors, self.upper, axis=0)
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
+        vectors = self.pipeline.fit_transform(vectors)
+        n_docs = vectors.shape[0]
+        self.distribution = np.array(
+            [np.bincount(v, minlength=len(self.bins)) / n_docs
+             for v in vectors.T])
+
+    def transform(self, vectors):
+        assert self.distribution is not None
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
+        probabilities = []
+        n_dim = vectors.shape[1]
+        vectors = self.pipeline.transform(vectors)
+        for bins in vectors:
             pr = (self.distribution[np.arange(n_dim), bins]).mean() / len(bins)
             probabilities.append(pr)
         return -np.log(np.maximum(1e-10, np.array(probabilities)))
