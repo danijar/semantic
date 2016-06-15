c509f0346e16c4f0412e58f1a2c11aa30f8a1de6
Alexander Immer <ext-alexander.immer@here.com>
2016-06-15 02:14:39 +0200

    Add naive multinomial estimator

--- semantic/step/__init__.py
+++ semantic/step/__init__.py
@@ -2,4 +2,4 @@ from .step import Step
 from .doc2vec import Doc2Vec
 from .tfidf import TfIdf
 from .gmm import GMM
-from .multinomial import Multinomial
+from .multinomial import MultinomialNB
--- semantic/step/multinomial.py
+++ semantic/step/multinomial.py
@@ -1,11 +1,46 @@
-from numpy import percentile, clip, digitize, arange, count_nonzero, array
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline
 
 from semantic.step import Step
 
 
-class Multinomial(Step):
+class MultinomialNB(Step):
+
+    def __init__(self, percentile_threshold, bins):
+        assert bins > 0
+        bin_size = 1 / bins
+        self.bins = np.arange(bin_size, 1, bin_size)
+        self.lower = percentile_threshold
+        self.upper = 100 - percentile_threshold
+        scaler = MinMaxScaler()
+        discretizer = FunctionTransformer(Discretizer(self.bins))
+        self.pipeline = Pipeline([('scaler', scaler),
+                                  ('discretizer', discretizer)])
+
+    def fit(self, vectors):
+        self.lower_clip = np.percentile(vectors, self.lower, axis=0)
+        self.upper_clip = np.percentile(vectors, self.upper, axis=0)
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
+        vectors = self.pipeline.fit_transform(vectors)
+        n_docs = vectors.shape[0]
+        self.distribution = np.array(
+                [np.bincount(v, minlength=len(self.bins))/n_docs
+                 for v in vectors.T])
+
+    def transform(self, vectors):
+        assert self.distribution is not None
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
+        probabilities = []
+        n_dim = vectors.shape[1]
+        vectors = self.pipeline.transform(vectors)
+        for bins in vectors:
+            pr = np.product(self.distribution[np.arange(n_dim), bins])
+            probabilities.append(pr)
+        return np.array(probabilities)
+
+
+class MultinomialDEP(Step):
 
     def __init__(self, percentile_threshold, bins):
         self.lower = percentile_threshold
@@ -16,27 +51,27 @@ class Multinomial(Step):
                                   ('discretizer', discretizer)])
 
     def fit(self, vectors):
-        self.lower_clip = percentile(vectors, self.lower, axis=0)
-        self.upper_clip = percentile(vectors, self.upper, axis=0)
-        vectors = clip(vectors, self.lower_clip, self.upper_clip)
+        self.lower_clip = np.percentile(vectors, self.lower, axis=0)
+        self.upper_clip = np.percentile(vectors, self.upper, axis=0)
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
         self.transformed_vectors = self.pipeline.fit_transform(vectors)
 
     def transform(self, vectors):
         assert self.transformed_vectors is not None
-        vectors = clip(vectors, self.lower_clip, self.upper_clip)
+        vectors = np.clip(vectors, self.lower_clip, self.upper_clip)
         probabilities = []
         vectors = self.pipeline.transform(vectors)
         docs = self.transformed_vectors.shape[0]
         for x in vectors:
-            count = count_nonzero((self.transformed_vectors == x).all(axis=1))
+            count = np.count_nonzero((self.transformed_vectors == x).all(axis=1))
             pr = count / docs
             probabilities.append(pr)
-        return array(probabilities)
+        return np.array(probabilities)
 
 
 class Discretizer:
-    def __init__(self, bins=100):
-        self.bin_size = 1/bins
+    def __init__(self, bins):
+        self.bins = bins
 
     def __call__(self, X):
-        return digitize(X, bins=arange(self.bin_size, 1, self.bin_size))
+        return np.digitize(X, bins=self.bins)
